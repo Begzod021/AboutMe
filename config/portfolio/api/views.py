93c50fc77e1da6394fef148d4b833f72b9ddc0b1
Begzod <97680127+Begzod021@users.noreply.github.com>
2022-06-01 00:58:36 +0500

    new

--- config/portfolio/api/views.py
+++ config/portfolio/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from portfolio.models import *
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 class Aboutme(APIView):
 
     def get(self, request):
