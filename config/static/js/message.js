b16b6a6c4ac515fb7e22c86e0e5574ae052c7016
Begzod <97680127+Begzod021@users.noreply.github.com>
2022-06-02 22:01:25 +0500

    new

--- config/static/js/message.js
+++
@@ -1,6 +0,0 @@
-message_card = document.getElementById('close');
-console.log('success')
-message_card.addEventListener('click', ()=>{
-    console.log('add style')
-    document.getElementById('success_message').style.display='none'
-})
